f6793f3b035a190f155c7f20a1247a812dc154ad
willamyizidro <willamyizidro@hotmail.com>
2024-04-23 00:41:44 -0300

    .

--- back-end/init_mesas.py
+++ back-end/init_mesas.py
@@ -5,7 +5,7 @@ def criar_mesas():
     with app.app_context():
         # Popula o banco de dados com 100 mesas inicialmente abertas
         for i in range(1, 101):
-            Mesa.create(numero = i+1, status = "livre")
+            Mesa.create(numero = i, status = "livre")
 
 
 
